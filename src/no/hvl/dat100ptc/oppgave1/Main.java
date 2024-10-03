a19793b7efe7499b44105b8a2f938aba09533ff4
h573042 <jasmine.erlandsen@hotmail.com>
2024-10-03 14:28:31 +0200

    2 a

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -3,8 +3,10 @@ package no.hvl.dat100ptc.oppgave1;
 public class Main {
 
 	public static void main(String[] args) {
+		//a)
 		GPSPoint punkt = new GPSPoint (1, 2.0, 3.0, 5.0);
 		
+		//b)
 		System.out.println(punkt.getTime());
 		
 		punkt.setTime(2);
--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -9,21 +9,43 @@ public class GPSDataConverter {
 	private static int TIME_STARTINDEX = 11; 
 
 	public static int toSeconds(String timestr) {
+		//a)		
+		int tIndex = timestr.indexOf("T");
+		int firstColonIndex = timestr.indexOf(":");
+		int secondColongIndex = timestr.indexOf(":" , firstColonIndex + 1);
 		
-		int secs;
-		int hr, min, sec;
+		// bruk substring for å hente timer, minutter og sekunder
+		String timer = timestr.substring(tIndex + 1, firstColonIndex);
+		int hr = Integer.parseInt(timer);
+		// bruk parseInt for å konverterer String til int
+		String minutter = timestr.substring(firstColonIndex + 1, secondColongIndex);
+		int min = Integer.parseInt(minutter);
+		String sekunder = timestr.substring(secondColongIndex + 1, timestr.indexOf("."));
+		int sec = Integer.parseInt(sekunder);
+
+		// beregn total antall sekunder
+		int total = (hr * 3600) + (min * 60) + sec;
+		
+		// returner antall sekunder
+		return total;
+		
+		
 		
 		// TODO
-		throw new UnsupportedOperationException(TODO.method());
+		
 		
 	}
 
 	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
 
 		GPSPoint gpspoint;
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
+		int time = toSeconds(timeStr);
+		double latitude = Double.parseDouble(latitudeStr);
+		double longitude = Double.parseDouble(longitudeStr);
+		double elevation = Double.parseDouble(elevationStr);
+		
+		return new GPSPoint(time, latitude, longitude, elevation);
+		
 		
 	}
 	
