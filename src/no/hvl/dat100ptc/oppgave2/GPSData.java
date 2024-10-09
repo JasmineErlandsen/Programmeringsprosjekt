46e0fe204c76e80e49190afb9ef29967ecfbea8d
h573042 <jasmine.erlandsen@hotmail.com>
2024-10-09 14:27:54 +0200

    opprettet konstruktører, konvertert verdiene fra String til gjeldende datatype.

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -9,40 +9,53 @@ public class GPSData {
 	protected int antall = 0;
 
 	public GPSData(int antall) {
-		antall = 0;
-		
-		
-		// TODO
+		// konstruktør, oppretter tabellen med new
+		gpspoints = new GPSPoint[antall];
+		this.antall = 0;
+
 	}
 
 	public GPSPoint[] getGPSPoints() {
 		return this.gpspoints;
 	}
-	
+
 	protected boolean insertGPS(GPSPoint gpspoint) {
 
 		boolean inserted = false;
+
+		if (antall < gpspoints.length) {
+			gpspoints[antall] = gpspoint;
+			antall++;
+			
+			inserted = true;
+			return inserted;
+		}else {
+			return inserted;
+		}
 		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
-	
+		// TODO
+
 	}
 
 	public boolean insert(String time, String latitude, String longitude, String elevation) {
 
 		GPSPoint gpspoint;
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		// Konverterer verdiene i parametre fra String til int og double
+		int tid = Integer.parseInt(time);
+		double lat = Double.parseDouble(latitude);
+		double lon = Double.parseDouble(longitude);
+		double hoyde =Double.parseDouble(elevation);
 		
+		
+
 	}
 
 	public void print() {
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		for (int i = 0; i < antall; i++);
+		
+		System.out.println("====== GPS Data - START ======");
+		System.out.println(insertGPS);
+		System.out.println("====== GPS Data - SLUTT ======");
+		
 	}
 }
